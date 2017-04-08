0a3678aedcc4f9e31ace4658088ab8b1ac9c4df5
ilhamjalu <ilham.jalu00@gmail.com>
2017-04-08 21:36:38 +0700

    Next Commits

---
+++ Scripts/Pause.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour {
+
+    public Canvas canvas;
+    public Button pause;
+    public Button resume;
+    public Button exit;
+    public GameObject Menu;
+    public AudioSource Sound;
+
+	// Update is called once per frame
+	public void Berhenti () {
+        Menu.SetActive(true);
+        Time.timeScale = 0;
+        Sound.Stop();
+	}
+
+    public void Lanjut()
+    {
+        Menu.SetActive(false);
+        Time.timeScale = 1;
+        Sound.Play();
+    }
+
+    public void Keluar()
+    {
+        Application.Quit();
+    }
+}
